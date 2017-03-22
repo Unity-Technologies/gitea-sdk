01cf3f12e2865284b865a4e44ab0523a876c651f
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2017-03-22 01:11:27 +0100

    Linting and Copyright Notice

--- gitea/status.go
+++ gitea/status.go
@@ -1,3 +1,7 @@
+// Copyright 2017 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package gitea
 
 import (
@@ -7,16 +11,24 @@ import (
 	"time"
 )
 
+// StatusState holds the state of a Status
+// It can be "pending", "success", "error", "failure", and "warning"
 type StatusState string
 
 const (
+	// StatusPending is for when the Status is Pending
 	StatusPending StatusState = "pending"
+	// StatusSuccess is for when the Status is Success
 	StatusSuccess StatusState = "success"
-	StatusError   StatusState = "error"
+	// StatusError is for when the Status is Error
+	StatusError StatusState = "error"
+	// StatusFailure is for when the Status is Failure
 	StatusFailure StatusState = "failure"
+	// StatusWarning is for when the Status is Warning
 	StatusWarning StatusState = "warning"
 )
 
+// Status holds a single Status of a single Commit
 type Status struct {
 	ID          int64       `json:"id"`
 	State       StatusState `json:"status"`
@@ -29,6 +41,7 @@ type Status struct {
 	Updated     time.Time   `json:"updated_at"`
 }
 
+// CombinedStatus holds the combined state of several statuses for a single commit
 type CombinedStatus struct {
 	State      StatusState `json:"state"`
 	SHA        string      `json:"sha"`
@@ -39,6 +52,7 @@ type CombinedStatus struct {
 	URL        string      `json:"url"`
 }
 
+// CreateStatusOption holds the information needed to create a new Status for a Commit
 type CreateStatusOption struct {
 	State       StatusState `json:"state"`
 	TargetURL   string      `json:"target_url"`
@@ -46,10 +60,13 @@ type CreateStatusOption struct {
 	Context     string      `json:"context"`
 }
 
+// ListStatusesOption holds pagination information
 type ListStatusesOption struct {
 	Page int
 }
 
+// CreateStatus creates a new Status for a given Commit
+//
 // POST /repos/:owner/:repo/statuses/:sha
 func (c *Client) CreateStatus(owner, repo, sha string, opts CreateStatusOption) (*Status, error) {
 	body, err := json.Marshal(&opts)
@@ -61,12 +78,16 @@ func (c *Client) CreateStatus(owner, repo, sha string, opts CreateStatusOption) 
 		jsonHeader, bytes.NewReader(body), status)
 }
 
+// ListStatuses returns all statuses for a given Commit
+//
 // GET /repos/:owner/:repo/commits/:ref/statuses
 func (c *Client) ListStatuses(owner, repo, sha string, opts ListStatusesOption) ([]*Status, error) {
 	statuses := make([]*Status, 0, 10)
 	return statuses, c.getParsedResponse("GET", fmt.Sprintf("/repos/%s/%s/commits/%s/statuses?page=%d", owner, repo, sha, opts.Page), nil, nil, &statuses)
 }
 
+// GetCombinedStatus returns the CombinedStatus for a given Commit
+//
 // GET /repos/:owner/:repo/commits/:ref/status
 func (c *Client) GetCombinedStatus(owner, repo, sha string) (*CombinedStatus, error) {
 	status := &CombinedStatus{}
