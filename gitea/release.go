7f04eaacfe3a493f8c36be9fe4404311387934ef
Petrisor Lacatus <placatus@ptc.com>
2017-06-30 16:38:19 +0300

    Added the endpoint `GET /repos/:owner/:repo/releases/latest` that gets  the latest published full release for the repository. Draft releases and prereleases are not returned by this endpoint.
    Signed-off-by: Petrisor Lacatus <placatus@ptc.com>

--- gitea/release.go
+++ gitea/release.go
@@ -65,6 +65,15 @@ func (c *Client) GetReleaseAsset(user, repo string, releaseID int64, assetID int
 	return r, err
 }
 
+// GetLatestRelease gets the latest release in a repository
+func (c *Client) GetLatestRelease(user, repo string, id int64) (*Release, error) {
+	r := new(Release)
+	err := c.getParsedResponse("GET",
+		fmt.Sprintf("/repos/%s/%s/releases/latest", user, repo, id),
+		nil, nil, &r)
+	return r, err
+}
+
 // CreateReleaseOption options when creating a release
 type CreateReleaseOption struct {
 	TagName      string `json:"tag_name" binding:"Required"`
