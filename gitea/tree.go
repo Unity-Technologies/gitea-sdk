a9116696c05ac09c90869e13faa0995ad607fcaa
Kasi <KasiR1995@hotmail.co.uk>
2018-06-10 19:08:31 +0100

    Added Tree API functionality

---
+++ gitea/tree.go
@@ -0,0 +1,39 @@
+// Copyright 2018 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package gitea
+
+import (
+	"fmt"
+)
+
+// TreeEntry represents the subtree structure within a tree.
+type TreeEntry struct {
+	Path		string 		`json:"path"`
+	Mode		string 		`json:"mode"`
+	Type		string 		`json:"type"`
+	Size		int64 		`json:"size,omitempty"`
+	SHA		string		`json:"sha"`
+	URL		string		`json:"url"`
+}
+
+// Tree represents the tree structure.
+type Tree struct {
+	SHA		string		`json:"sha"`
+	URL		string		`json:"url"`
+	Entries		[]TreeEntry	`json:"tree,omitempty"`
+	Truncated 	bool		`json:"truncated"`
+}
+
+// GetTree gets information on a tree given the owner, repo, and sha or ref.
+func (c *Client) GetTree(user string, repo string, tree string, recursive bool) (*Tree, error) {
+	t := new(Tree)
+	var Path = fmt.Sprintf("/repos/%s/%s/trees/%s", user, repo, tree)
+	if recursive {
+		Path += "?recursive=1"
+	}
+	err := c.getParsedResponse("GET", Path, nil, nil, t)
+	return t, err
+}
+
