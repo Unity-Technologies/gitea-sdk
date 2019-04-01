09e50a90472eb822764c1fb0291b573a1797c05c
Richard Mahn <rich.mahn@unfoldingword.org>
2019-04-01 13:16:50 -0400

    Adds wrapper for blob content

--- gitea/git_blob.go
+++ gitea/git_blob.go
@@ -4,11 +4,35 @@
 
 package gitea
 
+import (
+	"bytes"
+	"code.gitea.io/gitea/modules/git"
+	"encoding/json"
+)
+
 // GitBlobResponse represents a git blob
-type GitBlobResponse struct {
-	Content  string `json:"content"`
+type BlobResponse struct {
+	Content  *BlobContentResponse `json:"content"`
 	Encoding string `json:"encoding"`
 	URL      string `json:"url"`
 	SHA      string `json:"sha"`
 	Size     int64  `json:"size"`
+}
+
+type BlobContentResponse struct {
+	Blob *git.Blob
+}
+
+func (bc *BlobContentResponse) MarshalJSON() ([]byte, error) {
+	reader, err := bc.Blob.DataAsync()
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(reader)
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(buf.Bytes())
 }
