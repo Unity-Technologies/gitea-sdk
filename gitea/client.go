bd1193329ee3861c04fe12ec8a8606481556492d
6543 <6543@obermui.de>
2020-01-07 02:14:34 +0100

    dont change library version

--- gitea/client.go
+++ gitea/client.go
@@ -21,7 +21,7 @@ var jsonHeader = http.Header{"content-type": []string{"application/json"}}
 
 // Version return the library version
 func Version() string {
-	return "0.11.0"
+	return "0.12.3"
 }
 
 // Client represents a Gitea API client.
