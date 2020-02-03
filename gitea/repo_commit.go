7867e7746eb7e802096c7df7ad9137a56bdb96da
6543 <6543@obermui.de>
2020-02-03 15:29:57 +0100

    add ListRepoCommits

--- gitea/repo_commit.go
+++ gitea/repo_commit.go
@@ -7,6 +7,7 @@ package gitea
 
 import (
 	"fmt"
+	"net/url"
 )
 
 // Identity for a person's identity like an author or committer
@@ -50,4 +51,27 @@ type Commit struct {
 func (c *Client) GetSingleCommit(user, repo, commitID string) (*Commit, error) {
 	commit := new(Commit)
 	return commit, c.getParsedResponse("GET", fmt.Sprintf("/repos/%s/%s/git/commits/%s", user, repo, commitID), nil, nil, &commit)
+}
+
+// ListCommitOptions list commit options
+type ListCommitOptions struct {
+	//SHA or branch to start listing commits from (usually 'master')
+	SHA string
+}
+
+// QueryEncode turns options into querystring argument
+func (opt *ListCommitOptions) QueryEncode() string {
+	query := make(url.Values)
+	if opt.SHA != "" {
+		query.Add("sha", opt.SHA)
+	}
+	return query.Encode()
+}
+
+// ListRepoCommits return list of commits from a repo
+func (c *Client) ListRepoCommits(user, repo string, opt ListCommitOptions) ([]*Commit, error) {
+	link, _ := url.Parse(fmt.Sprintf("/repos/%s/%s/commits", user, repo))
+	commits := make([]*Commit, 0, 10)
+	link.RawQuery = opt.QueryEncode()
+	return commits, c.getParsedResponse("GET", link.String(), nil, nil, &commits)
 }
