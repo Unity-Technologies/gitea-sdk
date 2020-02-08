bcf1b92578eb816a6333dcdaee68998155af604b
6543 <6543@obermui.de>
2020-02-08 16:37:27 +0100

    add DeleteOrgMembership

--- gitea/org_member.go
+++ gitea/org_member.go
@@ -3,3 +3,14 @@
 // license that can be found in the LICENSE file.
 
 package gitea
+
+import (
+	"fmt"
+	"net/url"
+)
+
+// DeleteOrgMembership remove a member from an organization
+func (c *Client) DeleteOrgMembership(org, user string) error {
+	_, err := c.getResponse("DELETE", fmt.Sprintf("/orgs/%s/members/%s", url.PathEscape(org), url.PathEscape(user)), nil, nil)
+	return err
+}
