b9b7c3a92f08abc762faa8ced05d66122a15e536
6543 <6543@obermui.de>
2020-02-08 16:50:27 +0100

    add ListOrgMembership

--- gitea/org_member.go
+++ gitea/org_member.go
@@ -14,3 +14,18 @@ func (c *Client) DeleteOrgMembership(org, user string) error {
 	_, err := c.getResponse("DELETE", fmt.Sprintf("/orgs/%s/members/%s", url.PathEscape(org), url.PathEscape(user)), nil, nil)
 	return err
 }
+
+// ListIssueOption list issue options
+type ListOrgMembershipOption struct {
+	ListOptions
+}
+
+// ListOrgMembership list an organization's members
+func (c *Client) ListOrgMembership(org string, opt ListOrgMembershipOption) ([]*User, error) {
+	opt.setDefaults()
+	users := make([]*User, 0, opt.PageSize)
+
+	link, _ := url.Parse(fmt.Sprintf("/orgs/%s/members", url.PathEscape(org)))
+	link.RawQuery = opt.getURLQuery().Encode()
+	return users, c.getParsedResponse("GET", link.String(), jsonHeader, nil, &users)
+}
