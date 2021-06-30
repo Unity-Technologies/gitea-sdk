92629dc786fff67f4052134b52b4be5b0c370324
6543 <6543@obermui.de>
2021-07-01 00:11:24 +0200

    add new structs

--- gitea/repo_tag.go
+++ gitea/repo_tag.go
@@ -18,6 +18,32 @@ type Tag struct {
 	TarballURL string      `json:"tarball_url"`
 }
 
+// AnnotatedTag represents an annotated tag
+type AnnotatedTag struct {
+	Tag          string                     `json:"tag"`
+	SHA          string                     `json:"sha"`
+	URL          string                     `json:"url"`
+	Message      string                     `json:"message"`
+	Tagger       *CommitUser                `json:"tagger"`
+	Object       *AnnotatedTagObject        `json:"object"`
+	Verification *PayloadCommitVerification `json:"verification"`
+}
+
+// AnnotatedTagObject contains meta information of the tag object
+type AnnotatedTagObject struct {
+	Type string `json:"type"`
+	URL  string `json:"url"`
+	SHA  string `json:"sha"`
+}
+
+// CreateTagOption options when creating a tag
+type CreateTagOption struct {
+	// required: true
+	TagName string `json:"tag_name" binding:"Required"`
+	Message string `json:"message"`
+	Target  string `json:"target"`
+}
+
 // ListRepoTagsOptions options for listing a repository's tags
 type ListRepoTagsOptions struct {
 	ListOptions
