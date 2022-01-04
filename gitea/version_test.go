635de1b8215ef584976bcee6067aa07a5a9be68d
6543 <6543@obermui.de>
2022-01-05 00:31:31 +0800

    Add ignoreVersion & manuall version set option (#560)
    be able to skip version check if needed.
    
    !!! Be careful, because using it incorrectly can result in infinite loops with pagination !!!
    
    Reviewed-on: https://gitea.com/gitea/go-sdk/pulls/560
    Reviewed-by: Andrew Thornton <art27@cantab.net>
    Reviewed-by: Norwin <noerw@noreply.gitea.io>

--- gitea/version_test.go
+++ gitea/version_test.go
@@ -18,6 +18,15 @@ func TestVersion(t *testing.T) {
 	assert.NoError(t, err)
 	assert.True(t, true, rawVersion != "")
 
-	assert.NoError(t, c.CheckServerVersionConstraint(">= 1.11.0"))
+	assert.NoError(t, c.checkServerVersionGreaterThanOrEqual(version1_11_0))
 	assert.Error(t, c.CheckServerVersionConstraint("< 1.11.0"))
+
+	c.serverVersion = version1_11_0
+	assert.Error(t, c.checkServerVersionGreaterThanOrEqual(version1_15_0))
+	c.ignoreVersion = true
+	assert.NoError(t, c.checkServerVersionGreaterThanOrEqual(version1_15_0))
+
+	c, err = NewClient(getGiteaURL(), newTestClientAuth(), SetGiteaVersion("1.12.123"))
+	assert.NoError(t, err)
+	assert.NoError(t, c.CheckServerVersionConstraint("=1.12.123"))
 }
